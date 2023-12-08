8fe17085984e63c3bd5d43d5f0d0c9e2a6f81713
JackWang0318 <whj_20170318@163.com>
2023-12-08 09:56:35 +0800

    build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Spinner } from "@chakra-ui/react";
+import { Spinner, Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <Spinner color="red.500" />
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </div>
   );
 }

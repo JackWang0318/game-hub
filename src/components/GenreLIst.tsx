6f8c3b139fa1292669bf19d2376a9f5190cf7c39
JackWang0318 <whj_20170318@163.com>
2023-12-09 16:40:15 +0800

    highlight selected genre

--- src/components/GenreLIst.tsx
+++ src/components/GenreLIst.tsx
@@ -9,9 +9,10 @@ import {
 import useGenres, { Genre } from "../hooks/useGenres";
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />;
@@ -26,6 +27,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               src={genre.image_background}
             />
             <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
               variant="link"
